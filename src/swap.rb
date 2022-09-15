6f803d3fdf8c78e6a46d79de3cd63a4c111ddea8
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-09-15 19:21:31 -0300

    automatted commit

--- src/swap.rb
+++ src/swap.rb
@@ -2,11 +2,20 @@ def format_header
   STRUCT[:header] = STRUCT[:header].split("\n").map { |line| "#{line}\n"}.push("\n")
 end
 
-def write_files
-  binding.pry
-  format_header
-  STRUCT[:files].each do |file_path|
-    File.open(file_path, 'r+').first(12).write(STRUCT[:header])
+def replace_header
+  file_path = STRUCT[:files].first
+  index = 0
+  File.open(file_path, "r+") do |file|
+    file.each_line do |line|
+      break if line == "\n"
+      file.write(STRUCT[:header][index])
+      index += 1
+    end
   end
+end
+
+def write_files
+  format_header
+  replace_header
 end
 
